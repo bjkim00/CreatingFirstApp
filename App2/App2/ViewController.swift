2c8809b900e2c260b0c624e8437408306acc1512
Byungjin Kim <43766458+bjkim00@users.noreply.github.com>
2018-12-17 01:32:04 -0700

    Add files via upload
    More work with Xcode and Swift

---
+++ App2/App2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App2
+//
+//  Created by BJ Kim on 10/4/18.
+//  Copyright © 2018 BJ Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
